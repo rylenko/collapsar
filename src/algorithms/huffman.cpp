1198af645d5462c989fae62dd990f42c323888b0
rylenko <rylenko@tuta.io>
2024-06-30 00:12:50 +0700

    core: Add core::LessPtr; huffman: Update Node constructors, add docs, some corrections

--- src/algorithms/huffman.cpp
+++ src/algorithms/huffman.cpp
@@ -11,35 +11,41 @@
 #include <utility>
 #include <vector>
 
+#include "../core/comparator.h"
 #include "../core/freq_counter.h"
 
 namespace algorithms {
 
 class Node {
 	public:
-		constexpr Node(unsigned char ch, size_t freq);
-		constexpr Node(
-			unsigned char ch, size_t freq, const Node* left, const Node* right);
+		// Creates a character node with the corresponding frequency. The left and
+		// right branches are `nullptr`.
+		constexpr Node(unsigned char ch, size_t freq) noexcept;
+
+		// Creates a grouping node with left and right branches. In the case of this
+		// constructor both cannot be `nullptr`, you need to use another constructor.
+		constexpr Node(const Node* left, const Node* right) noexcept;
 
 		// A node is less than another if its frequency is higher. That is, the most
 		// frequent node requires less weight.
 		friend constexpr bool operator<(const Node& x, const Node& y) noexcept;
 
-		constexpr size_t get_freq() const noexcept {
-			return freq_;
-		}
-
 	private:
-		const unsigned char ch_;
-		// Frequency of character. Must be greater than 0.
-		const size_t freq_;
-		const Node* const left_;
-		const Node* const right_;
+		// A character with a certain frequency or a random character if the left and
+		// right branches are set.
+		unsigned char ch_;
+		// Character frequency or the sum of the frequencies of the left and right
+		// branches. Must be greater than 0.
+		size_t freq_;
+
+		// They are either both `nullptr`, or they both point to other nodes.
+		const Node* left_;
+		const Node* right_;
 };
 
 class Tree {
 	public:
-		constexpr explicit Tree(const Node* const root) noexcept;
+		constexpr explicit Tree(const Node* root) noexcept;
 		constexpr explicit Tree(const core::FreqCounter& freq_counter) noexcept;
 
 	private:
@@ -68,21 +74,25 @@ void HuffmanDecompressor::decompress(
 	output << "This is Huffman decompressor." << std::endl;
 }
 
-constexpr Node::Node(const unsigned char ch, const size_t freq)
-	: Node(ch, freq, nullptr, nullptr) {}
+constexpr Node::Node(const unsigned char ch, const size_t freq) noexcept
+		: ch_{ch}, freq_{freq}, left_{nullptr}, right_{nullptr} {
+	assert(freq > 0 && "zero frequency is useless for the tree");
+}
 
-constexpr Node::Node(
-		const unsigned char ch,
-		const size_t freq,
-		const Node* const left,
-		const Node* const right)
-		: ch_{ch}, freq_{freq}, left_{left}, right_{right} {
-	// Zero frequency does not make sense.
-	assert(freq > 0);
+constexpr Node::Node(const Node* const left, const Node* const right) noexcept
+		: ch_{'\0'},
+		freq_{0},  // Will change in the constructor body after pointers validation.
+		left_{left},
+		right_{right} {
+	// A grouping node must contain at least one branch to obtain a non-zero
+	// frequency.
+	assert(nullptr != left && nullptr != right && "they must not be `nullptr`");
+	// Update frequency after branch pointers validation.
+	freq_ = left->freq_ + right->freq_;
 }
 
 constexpr bool operator<(const Node& x, const Node& y) noexcept {
-	return x.get_freq() > y.get_freq();
+	return x.freq_ > y.freq_;
 }
 
 constexpr Tree::Tree(const Node* const root) noexcept: root_{root} {}
@@ -93,28 +103,24 @@ constexpr Tree::Tree(
 	std::vector<const Node*> nodes;
 
 	// Create character nodes with corresponding frequencies greater than 0.
-	unsigned char ch{0};
+	unsigned char ch{'\0'};
 	do {
 		if (freq_counter[ch] > 0) {
 			nodes.push_back(new Node(ch, freq_counter[ch]));
 		}
-	} while (++ch != 0);
+	} while (0 != ++ch);
 
 	// Build a tree of nodes. The most frequent nodes have the shortest path.
 	while (nodes.size() > 1) {
-		// Sort nodes in non-descending order.
-		auto compare = [](const Node* const x, const Node* const y) {
-			return *x < *y;
-		};
-		std::sort(nodes.begin(), nodes.end(), compare);
+		// Sort nodes. See less operator implementation for more.
+		std::sort(nodes.begin(), nodes.end(), core::LessPtr<Node>{});
 
-		// Build a tree node.
+		// Group the last two elements into a grouping node.
 		const Node* const right{nodes.back()};
 		nodes.pop_back();
 		const Node* const left{nodes.back()};
 		nodes.pop_back();
-		const size_t freq{left->get_freq() + right->get_freq()};
-		nodes.push_back(new Node(ch, freq, left, right));
+		nodes.push_back(new Node{left, right});
 	}
 
 	// Replace current `nullptr` root with built root.
---
+++ src/core/comparator.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace core {
+
+// Like `std::less`, but with pointers dereferencing.
+template <typename T>
+struct LessPtr {
+	constexpr bool operator()(const T* const lhs, const T* const rhs) const {
+		return *lhs < *rhs;
+	}
+};
+
+}  // namespace core
