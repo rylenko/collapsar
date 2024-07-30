d57f3bc3c37c767d222232e109ca095b452bd4f2
rylenko <rylenko@tuta.io>
2024-07-31 01:42:28 +0700

    Test dummy compressor and decompressor creation

--- tests/algorithm/compressor_factory.cpp
+++ tests/algorithm/compressor_factory.cpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 
 #include "algorithm/compressor_factory.h"
+#include "algorithm/dummy.h"
 #include "algorithm/huffman.h"
 #include "core/compressor.h"
 #include "core/compressor_factory.h"
@@ -12,8 +13,13 @@ TEST(CompressorFactory, Creation) {
 	std::unique_ptr<core::CompressorFactory> factory =
 		std::make_unique<algorithm::CompressorFactory>();
 
+	// Test dummy compressor creation.
+	std::unique_ptr<core::Compressor> compressor =
+		factory->create("no compression");
+	EXPECT_TRUE(dynamic_cast<algorithm::DummyCompressor*>(compressor.get()));
+
 	// Test Huffman's compressor creation.
-	std::unique_ptr<core::Compressor> compressor = factory->create("huffman");
+	compressor = factory->create("huffman");
 	EXPECT_TRUE(dynamic_cast<algorithm::HuffmanCompressor*>(compressor.get()));
 
 	// Test unknown compressor creation.
--- tests/algorithm/decompressor_factory.cpp
+++ tests/algorithm/decompressor_factory.cpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 
 #include "algorithm/decompressor_factory.h"
+#include "algorithm/dummy.h"
 #include "algorithm/huffman.h"
 #include "core/decompressor.h"
 #include "core/decompressor_factory.h"
@@ -12,8 +13,13 @@ TEST(DecompressorFactory, Creation) {
 	std::unique_ptr<core::DecompressorFactory> factory =
 		std::make_unique<algorithm::DecompressorFactory>();
 
+	// Test dummy decompressor creation.
+	std::unique_ptr<core::Decompressor> decompressor =
+		factory->create("no decompression");
+	EXPECT_TRUE(dynamic_cast<algorithm::DummyDecompressor*>(decompressor.get()));
+
 	// Test Huffman's decompressor creation.
-	std::unique_ptr<core::Decompressor> decompressor = factory->create("huffman");
+	decompressor = factory->create("huffman");
 	EXPECT_TRUE(
 		dynamic_cast<algorithm::HuffmanDecompressor*>(decompressor.get()));
 
